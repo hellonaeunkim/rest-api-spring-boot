8ab15b0deace9e5412ac89f724e24ae5b8835765
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-19 12:41:50 +0900

    initial commit

---
+++ src/test/java/com/annovation/gc_coffee/GcCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annovation.gc_coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
